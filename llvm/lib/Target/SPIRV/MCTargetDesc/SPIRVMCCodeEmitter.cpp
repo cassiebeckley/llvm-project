f068ea05b040fc533fb0bc4cb61347de29990c4f
Steven Perron <stevenperron@google.com>
2024-11-01 10:51:02 -0400

    [SPIRV] Add intrinsics to implement vk::ext_instruction
    In HLSL, the `vk::ext_instruction` attribute is used to indicate that a
    function represent a particular SPIR-V instruction. The opcode for the
    instruction is a parameter to the function.
    
    The input to the backend is a declaration of a function with an
    attribute indicating which opcode and extended instruction set to use.
    The extended instruciton set can be empty, indicating that a core
    instruction is being used.
    Then calls to the function are replaced by a single SPIR-V
    instruction.
    
    When outputing assembly, the string for the opcode is not readily
    available, so we to use `!val` to fill in when the string is not known. This syntax is
    accepted by `spirv-as`.
    
    This still does not work when the extended instruction set is unknown to
    the backend. This should be improved in a follow up PR.

--- llvm/lib/Target/SPIRV/MCTargetDesc/SPIRVMCCodeEmitter.cpp
+++ llvm/lib/Target/SPIRV/MCTargetDesc/SPIRVMCCodeEmitter.cpp
@@ -48,6 +48,10 @@ public:
   void encodeUnknownType(const MCInst &MI, SmallVectorImpl<char> &CB,
                          SmallVectorImpl<MCFixup> &Fixups,
                          const MCSubtargetInfo &STI) const;
+  void encodeUnknownInstruction(const MCInst &inst,
+                                SmallVectorImpl<char> &vector,
+                                SmallVectorImpl<MCFixup> &vector_1,
+                                const MCSubtargetInfo &info) const;
 };
 
 } // end anonymous namespace
@@ -124,12 +128,36 @@ void SPIRVMCCodeEmitter::encodeUnknownType(const MCInst &MI,
     emitOperand(MI.getOperand(i), CB);
 }
 
+void SPIRVMCCodeEmitter::encodeUnknownInstruction(
+    const MCInst &MI, SmallVectorImpl<char> &CB,
+    SmallVectorImpl<MCFixup> &Fixups, const MCSubtargetInfo &STI) const {
+  // Encode the first 32 SPIR-V bytes with the number of args and the opcode.
+
+  const uint64_t OpCode = MI.getOperand(2).getImm();
+  const uint32_t NumWords =
+      MI.getNumOperands() + 1 -
+      1; // The opcode is not an operand, so we need to substract 1.
+  const uint32_t FirstWord = (NumWords << 16) | OpCode;
+  support::endian::write(CB, FirstWord, llvm::endianness::little);
+
+  unsigned NumOps = MI.getNumOperands();
+  emitOperand(MI.getOperand(1), CB);
+  emitOperand(MI.getOperand(0), CB);
+  for (unsigned i = 3; i < NumOps; ++i)
+    emitOperand(MI.getOperand(i), CB);
+}
+
 void SPIRVMCCodeEmitter::encodeInstruction(const MCInst &MI,
                                            SmallVectorImpl<char> &CB,
                                            SmallVectorImpl<MCFixup> &Fixups,
                                            const MCSubtargetInfo &STI) const {
   if (MI.getOpcode() == SPIRV::UNKNOWN_type) {
     encodeUnknownType(MI, CB, Fixups, STI);
+    return;
+  }
+
+  if (MI.getOpcode() == SPIRV::UNKNOWN_instruction) {
+    encodeUnknownInstruction(MI, CB, Fixups, STI);
     return;
   }
 
